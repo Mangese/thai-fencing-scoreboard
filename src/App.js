39684236e43f0adf9ca59ec09b0ab84631bcdd64
Mangese <chainarhong2@gmail.com>
2025-03-11 01:24:55 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Scoreboard from "./components/scoreboard"
+
+function App() {
+	return (
+		<div className="content-wrapper">
+			<div className="App">
+				<Scoreboard />
+			</div>
+		</div>
+	)
+}
+
+export default App
